1f232277c63f5a6e8ff2dee073f47b583682e244
Serwach <mateusz.serwach.1@gmail.com>
2020-07-16 16:34:46 +0200

    event created

--- Controller/Index/Test.php
+++ Controller/Index/Test.php
@@ -13,9 +13,11 @@ class Test extends \Magento\Framework\App\Action\Action
 		return parent::__construct($context);
 	}
 
-	public function execute()
-	{
-		echo "Hello World";
-		exit;
-	}
+    public function execute()
+    {
+        $textDisplay = new \Magento\Framework\DataObject(array('text' => 'blablabla'));
+        $this->_eventManager->dispatch('mageplaza_helloworld_display_text', ['mp_text' => $textDisplay]);
+        echo $textDisplay->getText();
+        exit;
+    }
 }
